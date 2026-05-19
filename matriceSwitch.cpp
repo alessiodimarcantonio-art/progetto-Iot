36e67189d892fbfede55101f38f417b6bbe729f2
Your Name <you@example.com>
2026-05-19 12:25:24 +0200

    fix

--- matriceSwitch.cpp
+++ matriceSwitch.cpp
@@ -19,49 +19,48 @@ int main()
             cout << "inserisci valori nella matrice" << endl;
             cin >> voti[i][j];
         }
-
-        do
-        {
-            cout << "===MATRICI===" << endl;
-            cout << "1. visualizza matrice" << endl;
-            cout << "2. visuaizza diagonale matrice" << endl;
-            cout << "3. visualizza valore maggiore" << endl;
-            cin >> scelta;
-            switch (scelta)
-            {
-            case 1:
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        cout << voti[i][j] << "\t";
-                    }
-                    cout << endl;
-                }
-                break;
-
-            case 2:
-                for (int i = 0; i < 3; i++)
-                {
-                    cout << "la diagonale e formata da: " << voti[i][i] << endl;
-                }
-                break;
-            case 3:
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        VAL = voti[i][j];
-                        if (VAL > MAX)
-                        {
-                            MAX = VAL;
-                        }
-                    }
-                }
-                cout << "Il valore massimo e' :" << MAX << endl;
-                break;
-            }
-        } while (scelta != 0);
-        return 0;
     }
+    do
+    {
+        cout << "===MATRICI===" << endl;
+        cout << "1. visualizza matrice" << endl;
+        cout << "2. visuaizza diagonale matrice" << endl;
+        cout << "3. visualizza valore maggiore" << endl;
+        cin >> scelta;
+        switch (scelta)
+        {
+        case 1:
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    cout << voti[i][j] << "\t";
+                }
+                cout << endl;
+            }
+            break;
+
+        case 2:
+            for (int i = 0; i < 3; i++)
+            {
+                cout << "la diagonale e formata da: " << voti[i][i] << endl;
+            }
+            break;
+        case 3:
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    VAL = voti[i][j];
+                    if (VAL > MAX)
+                    {
+                        MAX = VAL;
+                    }
+                }
+            }
+            cout << "Il valore massimo e' :" << MAX << endl;
+            break;
+        }
+    } while (scelta != 0);
+    return 0;
 }
